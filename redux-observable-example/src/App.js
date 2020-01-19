5b80ca7dacd9a99aec8f2593e4c0656f0fa6e2b6
Jay <jpca999@gmail.com>
2020-01-18 17:09:47 -0800

    4 -  no of beers showing up on the UI now

--- redux-observable-example/src/App.js
+++ redux-observable-example/src/App.js
@@ -2,25 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
+import BeerList from './components/BeersList';
 
 function App(props) {
   console.log('6 - inside App.js  props = ', props.name );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Jay - Happy <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BeerList />
     </div>
   );
 }
